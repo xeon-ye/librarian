324318c3991ec528ed9cf4ab75d6437ba02b8c5a
= <=>
2020-06-08 22:27:11 +0800

    add test sql

---
+++ presto-unified-query/src/main/resources/student.sql
@@ -0,0 +1,8 @@
+
+
+create table student(
+ `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键ID',
+name varchar(50),
+age int(3),
+sex int(1)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
